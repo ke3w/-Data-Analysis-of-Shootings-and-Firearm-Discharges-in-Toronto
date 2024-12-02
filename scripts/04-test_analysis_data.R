af8fbf9eb6e3c23c7553b72fa0dd52d84ad6d620
吴欣泽 <wuxinze@mac.home>
2024-12-02 10:42:13 -0500

    test update

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -17,19 +17,16 @@ analysis_data <- read_csv("../data/02-analysis_data/analysis_data.csv")
 #### Define tests ####
 tests <- test_that("Data integrity and structure tests", {
   # Test that the dataset has the expected number of rows
-  expect_true(nrow(analysis_data) > 0)  # Adjust according to your expected row count
+  expect_true(nrow(analysis_data) == 129)
   
   # Test that the dataset has expected number of columns after cleaning and removing some
-  expect_equal(ncol(analysis_data), 15)  # Adjust based on actual columns you retained
+  expect_equal(ncol(analysis_data), 15)
   
-  # Test that 'division' is a factor type, which replaced police divisions
-  expect_type(analysis_data$division, "integer")  # Factors are internally stored as integers
+  # Test that the 'death' column is double type
+  expect_type(analysis_data$death, "double")
   
-  # Test that the 'death' column is integer type
-  expect_type(analysis_data$death, "integer")
-  
-  # Test that the 'injuries' column is integer type
-  expect_type(analysis_data$injuries, "integer")
+  # Test that the 'injuries' column is double type
+  expect_type(analysis_data$injuries, "double")
   
   # Test that there are no missing values in the dataset
   expect_true(all(!is.na(analysis_data)))
